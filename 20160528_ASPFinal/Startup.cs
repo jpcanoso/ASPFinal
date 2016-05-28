cefeb87705e7e30ca9590443821e5d4dcace1b6d
João Canoso <jpcanoso@hotmail.com>
2016-05-28 17:22:52 +0100

    Add project files.

---
+++ 20160528_ASPFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_20160528_ASPFinal.Startup))]
+namespace _20160528_ASPFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
